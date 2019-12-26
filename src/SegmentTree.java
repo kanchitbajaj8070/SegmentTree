3bc02525f461ebe532e15773dfa8e09df1075432
kanchitbajaj8070 <31831699+kanchitbajaj8070@users.noreply.github.com>
2019-12-26 12:53:26 +0530

    Initial commit

---
+++ src/SegmentTree.java
@@ -0,0 +1,15 @@
+public class SegmentTree {
+/*
+normal array -range sum-> O(n)
+            update-O(1)
+cumulative sum array - range sum->O(1)
+                     update-O(n)
+* segment tree- range query -> O(logn)
+*           update-O(logn)
+*   segment tree is a full binary tree-> every node except leaves has 2 childs
+*   N leaf nodes , N-1 internal nodes
+*   TotaL-> 2N-1 ( N+N-1)nodes
+*
+*           */
+
+}
