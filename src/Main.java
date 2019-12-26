0b0b974b184a38aca5c88bf49e4e3402b6563c69
kanchitbajaj8070 <31831699+kanchitbajaj8070@users.noreply.github.com>
2019-12-26 13:12:32 +0530

    s

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
+SegmentTree tree= new SegmentTree(new int[]{3,8,7,6,-2,-8,4,9});
+    tree.display();
 
     }
 
--- src/SegmentTree.java
+++ src/SegmentTree.java
@@ -11,5 +11,48 @@ cumulative sum array - range sum->O(1)
 *   TotaL-> 2N-1 ( N+N-1)nodes
 *
 *           */
+private class Node
+{
+    int data;
+    int startInterval;
+    int endInterval;
+    Node left;
+    Node right;
+}
+private Node root;
+public SegmentTree(int[]arr)
+{
+    root=construct( arr,0,arr.length-1);
+}
 
+    private Node construct(int[] arr, int s, int e) {
+if(s==e)
+{
+    Node leaf=new Node();
+    leaf.data=arr[s];
+    leaf.startInterval=s;
+    leaf.endInterval=e;
+    return leaf;
+}
+    Node nn= new Node();
+    nn.startInterval=s;
+    nn.endInterval=e;
+    int m=(s+e)/2;
+nn.left= construct(arr,s,m);
+nn.right=construct(arr,m+1,e);
+nn.data=nn.left.data+nn.right.data;
+return nn;
+}
+public void display()
+{
+    display(root);
+}
+
+    private void display(Node root) {
+if(root==null)
+    return;
+        System.out.println(root.data+" -> ["+root.startInterval+" , "+root.endInterval +"]");
+        display(root.left);
+        display(root.right);
+}
 }
